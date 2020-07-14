cc61b034ebbb363da59a56bf17f44c4c9b6be3f6
Cristian Molina <cmutep@gmail.com>
2020-07-14 10:51:44 -0600

    added alarm logic -- project finished

--- ghome.py
+++ ghome.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import datetime
+import time
 
 
 def cast_start():
 	payload = {
 	    "device": "Eril Display", 
-	    "source": "https://www.youtube.com/watch?time_continue=11&v=XdiGdC7K4sI&feature=emb_logo",
+	    "source": "https://www.youtube.com/watch?v=mshYP5KgzOY",
 	    "type": "remote"
 	}
 	r = requests.post('http://ar.local:3000/cast/', json=payload)
@@ -15,16 +17,15 @@ def cast_start():
 	#print(r)
 	print(r.text)
 
-def cast_stop():
-	payload = {
-        "device": "Eril Display",
-        "force": True
-}
-	r = requests.post('http://ar.local:3000/cast/', json=payload)
+#Asks for the time you want woken up and plays the video from cast_start method
+#at the specified time
+def wake_me_up_at(hr,min,sec):
+	now = datetime.datetime.now()
+	alarm_time = datetime.datetime.combine(now.date(), datetime.time(hr, min,sec))
+	time.sleep((alarm_time - now).total_seconds())
+	cast_start()
 
-	print("Status code: ", r.status_code)
-	print("Printing Entire Post Request")
-	#print(r)
-	print(r.text)
+wake_me_up_at(9,49,20)
 
-cast_start()
+
+
