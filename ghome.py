a7085732151b2f09786ef4cd65175c9706428435
Cristian Molina <cmutep@gmail.com>
2020-07-14 14:15:41 -0600

    Fixed a bug where the alarm would not work for any dates in the future

--- ghome.py
+++ ghome.py
@@ -3,7 +3,6 @@ import json
 import datetime
 import time
 
-
 def cast_start():
 	payload = {
 	    "device": "Eril Display", 
@@ -17,15 +16,30 @@ def cast_start():
 	#print(r)
 	print(r.text)
 
+
+
 #Asks for the time you want woken up and plays the video from cast_start method
 #at the specified time
-def wake_me_up_at(hr,min,sec):
-	now = datetime.datetime.now()
-	alarm_time = datetime.datetime.combine(now.date(), datetime.time(hr, min,sec))
-	time.sleep((alarm_time - now).total_seconds())
+#def wake_me_up_at(alarm_hr,alarm_min):
+	# if for some reason this script is still running
+# after a year, we'll stop after 365 days
+def wake_me_up_at(hr,min):
+	for i in xrange(0,365):
+	    # sleep until 2AM
+	    t = datetime.datetime.today() #today's info
+	    future = datetime.datetime(t.year,t.month,t.day,hr,min) #alarm info
+	    if t.hour >= hr:
+	        future += datetime.timedelta(days=1)
+	        print(future)
+	        print("waiting. . .")
+	    time.sleep((future-t).seconds)
+		
+
 	cast_start()
 
-wake_me_up_at(9,49,20)
+
+
+wake_me_up_at(6,0)
 
 
 
