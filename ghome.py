b91ce1cb0070724f93dc3cb4ca22212fcf3f058c
Cristian Molina <cmutep@gmail.com>
2020-07-17 14:05:03 -0600

    can now cast audio to both of my devices at the same time

--- ghome.py
+++ ghome.py
@@ -1,45 +1,52 @@
 import requests
-import json
 import datetime
 import time
 
+
 def cast_start():
-	payload = {
-	    "device": "Eril Display", 
-	    "source": "https://www.youtube.com/watch?v=mshYP5KgzOY",
-	    "type": "remote"
-	}
-	r = requests.post('http://ar.local:3000/cast/', json=payload)
+    payload = {
+        "device": "Elric Brothers",
+        "source": "https://www.youtube.com/watch?v=mshYP5KgzOY",
+        "type": "remote"
+    }
+    r = requests.post('http://ar.local:3000/cast/', json=payload)
 
-	print("Status code: ", r.status_code)
-	print("Printing Entire Post Request")
-	#print(r)
-	print(r.text)
+    print("Status code: ", r.status_code)
+    print("Attempting to cast")
+    # print(r)
+    print(r.text)
 
 
+def cast_stop():
+    payload = {
+        "device": "Eril Display",
+        "force": True
+    }
+    print("attempting to stop cast")
+    r = requests.post('http://ar.local:3000/cast/stop', json=payload)
 
-#Asks for the time you want woken up and plays the video from cast_start method
-#at the specified time
-#def wake_me_up_at(alarm_hr,alarm_min):
-	# if for some reason this script is still running
+    print("Status code: ", r.status_code)
+    # print(r)
+    print(r.text)
+
+
+# Asks for the time you want woken up and plays the video from cast_start method
+# at the specified time
+# if for some reason this script is still running
 # after a year, we'll stop after 365 days
-def wake_me_up_at(hr,min):
-	for i in xrange(0,365):
-	    # sleep until 2AM
-	    t = datetime.datetime.today() #today's info
-	    future = datetime.datetime(t.year,t.month,t.day,hr,min) #alarm info
-	    if t.hour >= hr:
-	        future += datetime.timedelta(days=1)
-	        print(future)
-	        print("waiting. . .")
-	    time.sleep((future-t).seconds)
-		
+def wake_me_up_at(hr, min):
+    for i in range(0, 365):
+        # sleep until 2AM
+        t = datetime.datetime.today()  # today's info
+        future = datetime.datetime(
+            t.year, t.month, t.day, hr, min)  # alarm info
+        if t.hour >= hr:
+            future += datetime.timedelta(days=1)
+            print(future)
+            print("waiting. . .")
+        time.sleep((future - t).seconds)
+        print("trying to cast...")
 
-	cast_start()
+    cast_start()
 
-
-
-wake_me_up_at(6,0)
-
-
-
+cast_start()
