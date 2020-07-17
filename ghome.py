474168f832ea59dbeee2799b1f95bc2774b1ba5c
Cristian Molina <cmutep@gmail.com>
2020-07-17 15:02:43 -0600

    fixed a multiple cast attemp bug
    fixed a bug where the program tried running a function that should only run once 300+ times.

--- ghome.py
+++ ghome.py
@@ -2,24 +2,28 @@ import requests
 import datetime
 import time
 
+video = "https://www.youtube.com/watch?v=mshYP5KgzOY"
 
-def cast_start():
+
+def cast_to(device_name):
     payload = {
-        "device": "Elric Brothers",
-        "source": "https://www.youtube.com/watch?v=mshYP5KgzOY",
+        "device": str(device_name),
+        "source": video,
         "type": "remote"
     }
+    print("trying to cast...")
+
     r = requests.post('http://ar.local:3000/cast/', json=payload)
 
     print("Status code: ", r.status_code)
-    print("Attempting to cast")
-    # print(r)
+    if(r.status_code != 200):
+        knock_door_three_times(payload)
     print(r.text)
 
 
 def cast_stop():
     payload = {
-        "device": "Eril Display",
+        "device": "Elric Brothers",
         "force": True
     }
     print("attempting to stop cast")
@@ -34,19 +38,34 @@ def cast_stop():
 # at the specified time
 # if for some reason this script is still running
 # after a year, we'll stop after 365 days
-def wake_me_up_at(hr, min):
-    for i in range(0, 365):
-        # sleep until 2AM
-        t = datetime.datetime.today()  # today's info
-        future = datetime.datetime(
-            t.year, t.month, t.day, hr, min)  # alarm info
-        if t.hour >= hr:
-            future += datetime.timedelta(days=1)
-            print(future)
-            print("waiting. . .")
+def wake_me_up_at(hr, min, sec):
+    # sleep until 2AM
+    t = datetime.datetime.today()  # today's info
+    future = datetime.datetime(
+        t.year, t.month, t.day, hr, min, sec)  # alarm info
+    if t.hour >= hr:
+        future += datetime.timedelta(days=0)
+        print(future)
+        print("waiting. . .")
         time.sleep((future - t).seconds)
-        print("trying to cast...")
+        cast_to("Eril Display")
 
-    cast_start()
 
-cast_start()
+def knock_door_three_times(payload):
+    knock_door(payload)
+    knock_door(payload)
+    knock_door(payload)
+
+
+def knock_door(payload):
+    requests.post('http://ar.local:3000/cast/', json=payload)
+    time.sleep(3)
+
+
+def clear():
+    print('\n' * 100)
+clear()
+wake_me_up_at(14, 59, 50)
+# cast_to("Eril Display")
+
+# cast_stop()
